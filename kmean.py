eeb3b178bade5aada1fa450d12189921ecc5e0fd
Bryan Yang <kenshin2004528@hotmail.com>
2014-10-25 00:38:29 +0800

    Update kmean.py

--- kmean.py
+++ kmean.py
@@ -1,4 +1,6 @@
-'''python
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Above the run-comment and file encoding comment.
 #----below is Bryan's code----
 # for caculate the minnimal distance between every bill and center of group
 # 
@@ -46,4 +48,3 @@ if __name__=='__main__':
         print 'usage : kmean.py <path_to_originFile, path_to_targetFile, group_number> '
         sys.exit(1)
     get_dis_export(sys.argv[1],sys.argv[2],int(sys.argv[3]))
-'''
