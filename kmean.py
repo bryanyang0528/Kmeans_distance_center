780b5e7ac44baa6acd41e662a7d61872a2fb8ace
bryanyang0528 <kenshin2004528@hotmail.com>
2014-10-25 20:20:55 +0800

    revise input and output data format

--- kmean.py
+++ kmean.py
@@ -15,36 +15,40 @@ def kmean_distance(filename, group):
     k = KMeans(n_clusters = group)
     g = k.fit_predict(filename)  ##group
     distance = k.fit_transform(filename) ## caculate distance between point and "every" group center
-    g = np.column_stack((filename, g, np.arange(len(filename)))) ## combine raw data and    
+    g = np.column_stack((np.arange(len(filename)),g, np.zeros((len(filename),)) )) ## combine raw data and    
     for nrow in range(len(g)):
-        id = int(g[nrow,4]) ##catch the group id 
+        id = int(g[nrow,1]) ##catch the group id 
         d = distance[nrow,id] ## get the distance with point's own group center 
-        g[nrow,5] = d ## combine
+        g[nrow,2] = d ## combine
     ##g_8 is the result
-    cnt = Counter(g[:,4])
+    cnt = Counter(g[:,1])
     cnt = sorted(cnt.items(),key = itemgetter(0))
     print "total group: %s" % (group)
     print "cnt of each group %s" % (cnt)
     return g
 
 def export(result,outFile):
-    fmt = "%i,%1.3f,%1.3f,%1.3f,%i,%1.3f"
+    fmt = "%i,%i,%1.3f"
     with open(outFile,'wb') as f:
-        f.write("bill,x,y,z,group,distance\n")
+        f.write("SerialNumber,Group,Distance\n")
         np.savetxt(f, result,delimiter=',', fmt=fmt)    
         print "export result to %s" % (outFile)
 
-def get_dis_export(inFile,outFile,groupN):
+def importFile(inFile):
     inFile = scipy.loadtxt(inFile, delimiter = ",")
+    return inFile    
+
+    
+def getResult(inFile,outFile,groupN):
+    inFile = importFile(inFile)
     result = kmean_distance(inFile, groupN)
     export(result,outFile)    
-
 
 if __name__=='__main__':
     if len(sys.argv) != 4:
         print 'usage : kmean.py <path_to_originFile, path_to_targetFile, group_number> '
         sys.exit(1)
-    get_dis_export(sys.argv[1],sys.argv[2],int(sys.argv[3]))
+    getResult(sys.argv[1],sys.argv[2],int(sys.argv[3]))
 
     
 
